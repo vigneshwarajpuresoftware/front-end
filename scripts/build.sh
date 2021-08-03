a574e2af10efc8e3f8a8654d4f180848e1a9212b
A J Vigneshwar <86348043+vigneshwarajpuresoftware@users.noreply.github.com>
2021-08-03 12:36:05 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,10 +25,6 @@ echo $CODE_DIR
 
 REPO=${GROUP}/$(basename front-end);
 $DOCKER_CMD run --rm --privileged multiarch/qemu-user-static --reset -p yes
-$DOCKER_CMD curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-$DOCKER_CMD add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-$DOCKER_CMD apt-get update
-$DOCKER_CMD apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
-$DOCKER_CMD buildx build -t ${REPO}:${COMMIT} --platfrom linux,arm64,linux.amd64 --push .
+$DOCKER_CMD --version
 echo $REPO
 echo $COMMIT
