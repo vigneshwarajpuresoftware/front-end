1be7479b331ae9ce99db029ff79d6c313cb431ff
A J Vigneshwar <86348043+vigneshwarajpuresoftware@users.noreply.github.com>
2021-08-03 12:55:13 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -26,5 +26,9 @@ echo $CODE_DIR
 REPO=${GROUP}/$(basename front-end);
 $DOCKER_CMD run --rm --privileged multiarch/qemu-user-static --reset -p yes
 $DOCKER_CMD --version
+$DOCKER_CMD login --username=ajv21 -p=Jun21@2021
+$DOCKER_CMD buildx create --name frontendbuildkit
+$DOCKER_CMD buildx use frontendbuildkit
+$DOCKER_CMD buildx inspect --bootstrap
 echo $REPO
 echo $COMMIT
